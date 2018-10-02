e48405c7fa42cb620b96f75859cc42a8d8f62bbc
Karara Aidara <boubash4@gmail.com>
2018-10-02 16:48:44 +0000

    create model video

--- app/src/main/java/sadba/lab/com/senprof/model/Video.java
+++ app/src/main/java/sadba/lab/com/senprof/model/Video.java
@@ -1,4 +1,25 @@
 package sadba.lab.com.senprof.model;
 
 public class Video {
+    private String nom;
+    private String youtube_id;
+
+    public Video() {
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getYoutube_id() {
+        return youtube_id;
+    }
+
+    public void setYoutube_id(String youtube_id) {
+        this.youtube_id = youtube_id;
+    }
 }
