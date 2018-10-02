c136d413e0fb29c86c01dd35b68f8db67b21da9f
sadba <sadba86@gmail.com>
2018-10-02 15:34:55 +0000

    first commit

---
+++ app/src/main/java/sadba/lab/com/senprof/MainActivity.java
@@ -0,0 +1,94 @@
+package sadba.lab.com.senprof;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener{
+
+
+    private CardView tempsCard, notesCard, evalCard, infosCard;
+    android.support.v7.widget.Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tempsCard = findViewById(R.id.temps_card);
+        notesCard = findViewById(R.id.notes_card);
+        evalCard = findViewById(R.id.eval_card);
+        infosCard = findViewById(R.id.infos_card);
+
+        toolbar =  findViewById(R.id.toolbarHome);
+
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.collapsingtoolbar);
+        collapsingToolbar.setTitle("SENPROF");
+
+        //Drawer layout
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        View headerView = navigationView.getHeaderView(0);
+
+        //Add Click Listener to the cards
+        tempsCard.setOnClickListener(this);
+        notesCard.setOnClickListener(this);
+        evalCard.setOnClickListener(this);
+        infosCard.setOnClickListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.dossier_eleve) {
+            // Handle the camera action
+            //Intent intent = new Intent(this, DossierActivity.class);
+            //startActivity(intent);
+        } else if (id == R.id.nav_sad) {
+
+
+
+        } else if (id == R.id.nav_slideshow) {
+
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
