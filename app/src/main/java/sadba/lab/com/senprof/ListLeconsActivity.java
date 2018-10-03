227332608f9811d600e1ba08aedcf305e52264ef
Karara Aidara <boubash4@gmail.com>
2018-10-03 12:13:17 +0000

    initialize expandable list view

--- app/src/main/java/sadba/lab/com/senprof/ListLeconsActivity.java
+++ app/src/main/java/sadba/lab/com/senprof/ListLeconsActivity.java
@@ -2,12 +2,24 @@ package sadba.lab.com.senprof;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
+
+import java.util.List;
+
+import sadba.lab.com.senprof.adapter.ExpandAdapter;
+import sadba.lab.com.senprof.model.Lecon;
 
 public class ListLeconsActivity extends AppCompatActivity {
 
+    private ExpandableListView elv;
+    private List<Lecon> lecons;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_lecons);
+
+        elv = findViewById(R.id.LeconExpandId);
+
+        elv.setAdapter(new ExpandAdapter(this, lecons));
     }
 }
