a8d1f579c56fbfafc3303805de92da290226850d
Karara Aidara <boubash4@gmail.com>
2018-10-03 12:12:02 +0000

    create activities listleon and youtubedialog

---
+++ app/src/main/java/sadba/lab/com/senprof/ListLeconsActivity.java
@@ -0,0 +1,13 @@
+package sadba.lab.com.senprof;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListLeconsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_lecons);
+    }
+}
