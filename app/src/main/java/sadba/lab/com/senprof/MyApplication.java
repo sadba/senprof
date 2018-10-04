6509ef905a8fb43d89fc93f0bac406ba2b1b338d
Karara Aidara <boubash4@gmail.com>
2018-10-04 10:13:19 +0000

    add sharedpref

--- app/src/main/java/sadba/lab/com/senprof/MyApplication.java
+++ app/src/main/java/sadba/lab/com/senprof/MyApplication.java
@@ -1,6 +1,9 @@
 package sadba.lab.com.senprof;
 
 import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -12,14 +15,18 @@ import java.util.List;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import io.realm.RealmList;
 import sadba.lab.com.senprof.Api.JsonFetcher;
 import sadba.lab.com.senprof.model.Lecon;
 import sadba.lab.com.senprof.model.Video;
 
 public class MyApplication extends Application {
 
+    Realm realm;
     List<Video> videos = new ArrayList<>();
     List<Lecon> lecons = new ArrayList<>();
+    Boolean donneeIntialiser;
+    private static final String TAG = MyApplication.class.getCanonicalName();
 
     @Override
     public void onCreate() {
@@ -31,13 +38,32 @@ public class MyApplication extends Application {
                 .schemaVersion(0)
                 .build();
         Realm.setDefaultConfiguration(realmConfiguration);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        donneeIntialiser = sharedPreferences.getBoolean("donneesIntialisees", false);
+
+        if (!donneeIntialiser) {
+            try {
+                lecons = setUpLecon();
+            } catch (JSONException | IOException e) {
+                e.printStackTrace();
+            }
+            realm = Realm.getDefaultInstance();
+            realm.beginTransaction();
+            realm.copyToRealmOrUpdate(lecons);
+            realm.commitTransaction();
+
+            editor.putBoolean("niveauAjouter", true);
+            editor.apply();
+        }
     }
 
     private List<Lecon> setUpLecon() throws IOException, JSONException {
         JsonFetcher fetcher = new JsonFetcher(this, R.raw.leconsvideos);
         String fileContent = fetcher.toString();
         JSONObject tokner = new JSONObject(fileContent);
-       // RealmList<Video> videos = new RealmList<>();
+        RealmList<Video> videos = new RealmList<>();
         try {
             JSONArray jArray = tokner.getJSONArray("LeconsVideos");
             for (int i = 0; i < jArray.length(); i++) {
@@ -46,7 +72,7 @@ public class MyApplication extends Application {
                 lecon.setId(l.getInt("id"));
                 lecon.setNom(l.getString("nom"));
                 //lecon.setChapitre_id(l.getInt("chapitre_id"));
-                lecon.setMatiere_id(l.getInt("matiere_id"));
+                //lecon.setMatiere_id(l.getInt("matiere_id"));
 
                 JSONArray videoArray = l.getJSONArray("videos");
 
@@ -62,11 +88,9 @@ public class MyApplication extends Application {
                 lecons.add(lecon);
 
             }
-
-
         } catch (Exception e) {
             e.printStackTrace();
-            //Log.d(TAG, "setUpLecon: " + e.getLocalizedMessage());
+            Log.d(TAG, "setUpLecon: " + e.getLocalizedMessage());
         }
         return lecons;
     }
