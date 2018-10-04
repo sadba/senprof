0d83c265a2d4512bfe8d9cf4498feae6bebc4905
Karara Aidara <boubash4@gmail.com>
2018-10-04 10:10:27 +0000

    init realm and get data

--- app/src/main/java/sadba/lab/com/senprof/ListLeconsActivity.java
+++ app/src/main/java/sadba/lab/com/senprof/ListLeconsActivity.java
@@ -6,19 +6,23 @@ import android.widget.ExpandableListView;
 
 import java.util.List;
 
+import io.realm.Realm;
 import sadba.lab.com.senprof.adapter.ExpandAdapter;
 import sadba.lab.com.senprof.model.Lecon;
 
 public class ListLeconsActivity extends AppCompatActivity {
 
-    private ExpandableListView elv;
-    private List<Lecon> lecons;
+    Realm realm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_lecons);
+        Realm.init(this);
 
-        elv = findViewById(R.id.LeconExpandId);
+        ExpandableListView elv = findViewById(R.id.LeconExpandId);
+        realm = Realm.getDefaultInstance();
+        List<Lecon> lecons = realm.where(Lecon.class).findAll();
 
         elv.setAdapter(new ExpandAdapter(this, lecons));
     }
