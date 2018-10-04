ee0e3a0826e8934e63eae268810c7213538d56a1
Karara Aidara <boubash4@gmail.com>
2018-10-04 12:15:53 +0000

    add youtube Api

--- app/src/main/java/sadba/lab/com/senprof/YoutubeDialogActivity.java
+++ app/src/main/java/sadba/lab/com/senprof/YoutubeDialogActivity.java
@@ -1,13 +1,151 @@
 package sadba.lab.com.senprof;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class YoutubeDialogActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import io.realm.Realm;
+import io.realm.RealmList;
+import sadba.lab.com.senprof.Api.Config;
+import sadba.lab.com.senprof.adapter.VideoAdapter;
+import sadba.lab.com.senprof.model.Lecon;
+import sadba.lab.com.senprof.model.Video;
+
+public class YoutubeDialogActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_REQUEST = 1;
+    YouTubePlayer player;
+    Realm realm;
+    private YouTubePlayerView youTubePlayerView;
+    private MyPlayerStateChangeListener playerStateChangeListener;
+    private MyPlaybackEventListener playbackEventListener;
+    private String youtubeId, videoName;
+    private int matiere_id;
+    private TextView mVideoName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube_dialog);
+
+        Realm.init(this);
+        realm = Realm.getDefaultInstance();
+
+        youTubePlayerView = findViewById(R.id.youtube_view);
+        youtubeId = getIntent().getStringExtra("youtubekey");
+        videoName = getIntent().getStringExtra("videoName");
+        matiere_id = getIntent().getIntExtra("leconId", -1);
+
+        RecyclerView recView = findViewById(R.id.recyclerVideo);
+
+        /*Lecon results = realm.where(Lecon.class).equalTo("id", matiere_id).findFirst();
+        RealmList<Video> videos = results.getVideos();
+
+        recView.setLayoutManager(new LinearLayoutManager(this));
+        recView.setAdapter(new VideoAdapter(this, videos));
+        Toast.makeText(this, results.getNom(), Toast.LENGTH_SHORT).show();
+
+        mVideoName = findViewById(R.id.leconTxt);
+        mVideoName.setText(videoName);*/
+
+        youTubePlayerView.initialize(Config.getApiKey(), this);
+        playerStateChangeListener = new MyPlayerStateChangeListener();
+        playbackEventListener = new MyPlaybackEventListener();
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+        this.player = youTubePlayer;
+        youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        youTubePlayer.setPlaybackEventListener(playbackEventListener);
+        if (!b) {
+            youTubePlayer.loadVideo(youtubeId);
+        }
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    private final class MyPlayerStateChangeListener implements YouTubePlayer.PlayerStateChangeListener {
+
+        @Override
+        public void onLoading() {
+            // Called when the player is loading a video
+            // At this point, it's not ready to accept commands affecting playback such as play() or pause()
+        }
+
+        @Override
+        public void onLoaded(String s) {
+            // Called when a video is done loading.
+            // Playback methods such as play(), pause() or seekToMillis(int) may be called after this callback.
+        }
+
+        @Override
+        public void onAdStarted() {
+            // Called when playback of an advertisement starts.
+        }
+
+        @Override
+        public void onVideoStarted() {
+            // Called when playback of the video starts.
+        }
+
+        @Override
+        public void onVideoEnded() {
+            // Called when the video reaches its end.
+        }
+
+        @Override
+        public void onError(YouTubePlayer.ErrorReason errorReason) {
+            // Called when an error occurs.
+        }
+    }
+
+    private final class MyPlaybackEventListener implements YouTubePlayer.PlaybackEventListener {
+
+        @Override
+        public void onPlaying() {
+            // Called when playback starts, either due to user action or call to play().
+            // showMessage("Playing");
+        }
+
+        @Override
+        public void onPaused() {
+            // Called when playback is paused, either due to user action or call to pause().
+            //showMessage("Paused");
+        }
+
+        @Override
+        public void onStopped() {
+            // Called when playback stops for a reason other than being paused.
+            //showMessage("Stopped");
+        }
+
+        @Override
+        public void onBuffering(boolean b) {
+            // Called when buffering starts or ends.
+        }
+
+        @Override
+        public void onSeekTo(int i) {
+            // Called when a jump in playback position occurs, either
+            // due to user scrubbing or call to seekRelativeMillis() or seekToMillis()
+        }
     }
 }
