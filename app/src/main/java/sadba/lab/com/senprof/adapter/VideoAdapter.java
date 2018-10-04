57a9c2d5542e26a66dff0b6581fc571bcfb62e03
Karara Aidara <boubash4@gmail.com>
2018-10-04 12:19:05 +0000

    adater for video

--- app/src/main/java/sadba/lab/com/senprof/adapter/VideoAdapter.java
+++ app/src/main/java/sadba/lab/com/senprof/adapter/VideoAdapter.java
@@ -1,4 +1,44 @@
 package sadba.lab.com.senprof.adapter;
 
-public class VideoAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+import sadba.lab.com.senprof.R;
+import sadba.lab.com.senprof.model.Video;
+
+public class VideoAdapter extends RecyclerView.Adapter<VideoViewHolder> {
+    private LayoutInflater mInflater;
+    private Context context;
+    private List<Video> videos;
+
+    public VideoAdapter(Context context, List<Video> videos) {
+        this.context = context;
+        this.videos = videos;
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = mInflater.inflate(R.layout.raw_child, viewGroup, false);
+        return new VideoViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoViewHolder videoViewHolder, int i) {
+        Video video = videos.get(i);
+        VideoViewHolder.bindVideo(video);
+    }
+
+    @Override
+    public int getItemCount() {
+        return videos.size();
+    }
 }
--- app/src/main/java/sadba/lab/com/senprof/adapter/VideoViewHolder.java
+++ app/src/main/java/sadba/lab/com/senprof/adapter/VideoViewHolder.java
@@ -1,4 +1,25 @@
 package sadba.lab.com.senprof.adapter;
 
-public class VideoViewHolder {
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import sadba.lab.com.senprof.R;
+import sadba.lab.com.senprof.model.Video;
+
+public class VideoViewHolder extends RecyclerView.ViewHolder {
+
+    private static TextView mVideoTitle;
+
+    public VideoViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        mVideoTitle = itemView.findViewById(R.id.VideoTitle);
+
+    }
+
+    static void bindVideo(Video video) {
+        mVideoTitle.setText(video.getNom());
+    }
 }
